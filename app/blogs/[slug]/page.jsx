48bfac588c3bdc26f4d7afc5f5537421b4acfc7f
Subha Patra <56377135+subha-wp@users.noreply.github.com>
2023-11-24 17:46:07 +0530

    Update page.jsx

--- app/blogs/[slug]/page.jsx
+++ app/blogs/[slug]/page.jsx
@@ -5,118 +5,123 @@ const body = (
   <p>
     {" "}
     Introduction Hey there! Wondering what the weather has in store for you?
-    Well, you're in luck because I've got the most recent weather forecast
-    report hot off the press. Whether you're planning your weekend getaway or
-    just deciding what to wear tomorrow, having the latest weather information
-    is crucial. Mother Nature can be quite unpredictable, but with this
-    up-to-date report, you'll be able to stay one step ahead. No more getting
-    caught in unexpected rain showers or leaving the house unprepared for a
-    sudden heatwave. So, grab a cup of coffee, sit back, and let's dive into the
-    most recent weather forecast. Rain or shine, I've got you covered! Current
-    Temperature and Humidity Readings Across Singapore Currently, the
-    temperature in Singapore is 28 degrees Celsius with a humidity level of 80%.
-    The weather is expected to remain warm and humid throughout the day,
-    reaching a high of 31 degrees Celsius in the afternoon. This high humidity
-    can make it feel even warmer than it actually is, so it's important to stay
-    hydrated and seek shade when necessary. Keep in mind that these readings can
-    vary depending on your location within Singapore, as different areas may
+    Well&#39; you&apos;re in luck because I&apos;ve got the most recent weather
+    forecast report hot off the press. Whether you&apos;re planning your weekend
+    getaway or just deciding what to wear tomorrow&#39; having the latest
+    weather information is crucial. Mother Nature can be quite
+    unpredictable&#39; but with this up-to-date report&#39; you&apos;ll be able
+    to stay one step ahead. No more getting caught in unexpected rain showers or
+    leaving the house unprepared for a sudden heatwave. So&#39; grab a cup of
+    coffee&#39; sit back&#39; and let&apos;s dive into the most recent weather
+    forecast. Rain or shine&#39; I&apos;ve got you covered! Current Temperature
+    and Humidity Readings Across Singapore Currently&#39; the temperature in
+    Singapore is 28 degrees Celsius with a humidity level of 80%. The weather is
+    expected to remain warm and humid throughout the day&#39; reaching a high of
+    31 degrees Celsius in the afternoon. This high humidity can make it feel
+    even warmer than it actually is&#39; so it&apos;s important to stay hydrated
+    and seek shade when necessary. Keep in mind that these readings can vary
+    depending on your location within Singapore&#39; as different areas may
     experience slightly different temperatures and humidity levels. Wind
     Conditions and Speeds According to Regional Weather Stations When it comes
-    to wind conditions, staying informed is crucial. The most recent weather
+    to wind conditions&#39; staying informed is crucial. The most recent weather
     forecast report provides a detailed analysis of the current wind speeds
-    across various regions. Here's what you need to know: - Region A: Wind
+    across various regions. Here&apos;s what you need to know: - Region A: Wind
     speeds are expected to reach up to 25 miles per hour throughout the day.
     This could lead to potential gusts and difficulties in outdoor activities. -
-    Region B: Mild winds are projected, with speeds averaging around 10 miles
-    per hour. While not particularly strong, it's still important to be
-    cautious, especially when engaging in activities near open spaces. - Region
-    C: The weather station in this region has recorded high wind speeds of up to
-    40 miles per hour. It is advisable to take necessary precautions, such as
-    securing loose objects and avoiding outdoor activities that may pose risks.
-    Understanding the wind conditions and speeds in your specific region allows
-    you to plan your day accordingly. Whether it's determining the best time for
-    a hike or deciding if it's safe for outdoor sports, staying informed
-    empowers you to make smart choices that prioritize your safety and
-    well-being. Amount of Rainfall and Precipitation Recorded Over the Last 24
-    Hours In the last 24 hours, the region has experienced a significant amount
-    of rainfall and precipitation. The weather report indicates that a total of
-    2 inches of rain has fallen, making it one of the wettest days this year.
-    The rainfall was accompanied by strong winds, reaching up to 25 miles per
-    hour, causing localized flooding in some areas. This heavy rainfall is
-    expected to continue throughout the day, with an additional inch of rain
-    predicted by tonight. It is important for residents to take necessary
-    precautions, such as avoiding low-lying areas and ensuring proper drainage
-    systems are in place to prevent any potential damage caused by the excessive
-    amount of rainfall. Stay tuned for further updates on the weather conditions
-    and any advisories issued by local authorities. Air Pressure Readings
-    Signaling Any Weather Changes The most recent weather forecast report
-    provides valuable information on air pressure readings, which can signal any
-    weather changes on the horizon. Air pressure is a key factor in determining
-    the type of weather we can expect, as it is directly related to the movement
-    of air masses and the formation of weather systems. ### High Pressure
-    Systems When air pressure is high, it indicates the presence of a
-    high-pressure system. High-pressure systems typically bring fair weather
-    conditions, as the sinking air inhibits the formation of clouds and
-    precipitation. This means that areas experiencing high air pressure can
-    expect clear skies, sunny days, and calm winds. ### Low Pressure Systems On
-    the other hand, low air pressure signifies the presence of a low-pressure
-    system. Low-pressure systems are associated with unsettled weather
-    conditions, as the rising air creates instability and leads to the formation
-    of clouds and precipitation. Areas experiencing low air pressure can
-    anticipate cloudy skies, strong winds, and the potential for rain or storms.
-    By monitoring air pressure readings, meteorologists can forecast the
+    Region B: Mild winds are projected&#39; with speeds averaging around 10
+    miles per hour. While not particularly strong&#39; it&apos;s still important
+    to be cautious&#39; especially when engaging in activities near open spaces.
+    - Region C: The weather station in this region has recorded high wind speeds
+    of up to 40 miles per hour. It is advisable to take necessary
+    precautions&#39; such as securing loose objects and avoiding outdoor
+    activities that may pose risks. Understanding the wind conditions and speeds
+    in your specific region allows you to plan your day accordingly. Whether
+    it&apos;s determining the best time for a hike or deciding if it&apos;s safe
+    for outdoor sports&#39; staying informed empowers you to make smart choices
+    that prioritize your safety and well-being. Amount of Rainfall and
+    Precipitation Recorded Over the Last 24 Hours In the last 24 hours&#39; the
+    region has experienced a significant amount of rainfall and precipitation.
+    The weather report indicates that a total of 2 inches of rain has
+    fallen&#39; making it one of the wettest days this year. The rainfall was
+    accompanied by strong winds&#39; reaching up to 25 miles per hour&#39;
+    causing localized flooding in some areas. This heavy rainfall is expected to
+    continue throughout the day&#39; with an additional inch of rain predicted
+    by tonight. It is important for residents to take necessary precautions&#39;
+    such as avoiding low-lying areas and ensuring proper drainage systems are in
+    place to prevent any potential damage caused by the excessive amount of
+    rainfall. Stay tuned for further updates on the weather conditions and any
+    advisories issued by local authorities. Air Pressure Readings Signaling Any
+    Weather Changes The most recent weather forecast report provides valuable
+    information on air pressure readings&#39; which can signal any weather
+    changes on the horizon. Air pressure is a key factor in determining the type
+    of weather we can expect&#39; as it is directly related to the movement of
+    air masses and the formation of weather systems. ### High Pressure Systems
+    When air pressure is high&#39; it indicates the presence of a high-pressure
+    system. High-pressure systems typically bring fair weather conditions&#39;
+    as the sinking air inhibits the formation of clouds and precipitation. This
+    means that areas experiencing high air pressure can expect clear skies&#39;
+    sunny days&#39; and calm winds. ### Low Pressure Systems On the other
+    hand&#39; low air pressure signifies the presence of a low-pressure system.
+    Low-pressure systems are associated with unsettled weather conditions&#39;
+    as the rising air creates instability and leads to the formation of clouds
+    and precipitation. Areas experiencing low air pressure can anticipate cloudy
+    skies&#39; strong winds&#39; and the potential for rain or storms. By
+    monitoring air pressure readings&#39; meteorologists can forecast the
     movement of these pressure systems and provide accurate predictions for the
     weather in different regions. This information is crucial for individuals
-    and businesses alike, as it helps them make informed decisions and prepare
-    for any weather-related challenges that may arise. Whether planning outdoor
-    activities, adjusting transportation routes, or making agricultural
-    decisions, having access to the latest air pressure readings allows us to
-    stay ahead of the weather and adapt accordingly. UV Index Levels - Time to
-    Slather on the Sunscreen! The latest weather report has just come in, and
-    it's time to break out the sunscreen! The UV index levels for today are
-    expected to be high, which means that the sun's rays will be particularly
-    strong and can cause damage to your skin. It's important to take precautions
-    and protect yourself from harmful UV radiation. Here are a few tips to keep
-    in mind: 1. Wear Sunscreen: Choose a broad-spectrum sunscreen with an SPF of
-    30 or higher. Apply it generously to all exposed skin, including your face,
-    neck, arms, and legs. Don't forget to reapply every two hours, especially if
-    you're spending a lot of time outdoors. 2. Seek Shade: If possible, try to
-    stay in the shade during the peak hours of the day when the sun is
-    strongest, usually between 10 am and 4 pm. This will help reduce your
-    exposure to harmful UV rays. 3. Wear Protective Clothing: Cover up as much
-    as possible by wearing long-sleeved shirts, pants, and a wide-brimmed hat.
-    This will provide an extra layer of protection for your skin. 4. Use
-    Sunglasses: Protect your eyes from the sun's rays by wearing sunglasses that
-    block 100% of UVA and UVB rays. Look for sunglasses with a UV 400 rating to
-    ensure maximum protection. Remember, even on cloudy days, UV rays can still
-    penetrate through the clouds and cause damage to your skin. So make sure to
-    take these precautions and stay safe under the sun! The Week Ahead: 7-Day
-    Weather Forecast Summary for Singapore Let's take a look at the week ahead
-    and see what the weather has in store for Singapore. Here is a summary of
-    the 7-day weather forecast: Monday: Expect a mix of sun and clouds with a
-    high temperature of 32°C (90°F) and a low of 26°C (79°F). There is a slight
+    and businesses alike&#39; as it helps them make informed decisions and
+    prepare for any weather-related challenges that may arise. Whether planning
+    outdoor activities&#39; adjusting transportation routes&#39; or making
+    agricultural decisions&#39; having access to the latest air pressure
+    readings allows us to stay ahead of the weather and adapt accordingly. UV
+    Index Levels - Time to Slather on the Sunscreen! The latest weather report
+    has just come in&#39; and it&apos;s time to break out the sunscreen! The UV
+    index levels for today are expected to be high&#39; which means that the
+    sun&apos;s rays will be particularly strong and can cause damage to your
+    skin. It&apos;s important to take precautions and protect yourself from
+    harmful UV radiation. Here are a few tips to keep in mind: 1. Wear
+    Sunscreen: Choose a broad-spectrum sunscreen with an SPF of 30 or higher.
+    Apply it generously to all exposed skin&#39; including your face&#39;
+    neck&#39; arms&#39; and legs. Don&apos;t forget to reapply every two
+    hours&#39; especially if you&apos;re spending a lot of time outdoors. 2.
+    Seek Shade: If possible&#39; try to stay in the shade during the peak hours
+    of the day when the sun is strongest&#39; usually between 10 am and 4 pm.
+    This will help reduce your exposure to harmful UV rays. 3. Wear Protective
+    Clothing: Cover up as much as possible by wearing long-sleeved shirts&#39;
+    pants&#39; and a wide-brimmed hat. This will provide an extra layer of
+    protection for your skin. 4. Use Sunglasses: Protect your eyes from the
+    sun&apos;s rays by wearing sunglasses that block 100% of UVA and UVB rays.
+    Look for sunglasses with a UV 400 rating to ensure maximum protection.
+    Remember&#39; even on cloudy days&#39; UV rays can still penetrate through
+    the clouds and cause damage to your skin. So make sure to take these
+    precautions and stay safe under the sun! The Week Ahead: 7-Day Weather
+    Forecast Summary for Singapore Let&apos;s take a look at the week ahead and
+    see what the weather has in store for Singapore. Here is a summary of the
+    7-day weather forecast: Monday: Expect a mix of sun and clouds with a high
+    temperature of 32°C (90°F) and a low of 26°C (79°F). There is a slight
     chance of afternoon showers. Tuesday: It will be mostly sunny with a high of
     33°C (91°F) and a low of 27°C (81°F). The chance of rain is minimal.
-    Wednesday: Another day of sunshine is in store, with temperatures reaching a
-    high of 34°C (93°F) and a low of 28°C (82°F). No rain is expected. Thursday:
-    The heat continues, with a high of 35°C (95°F) and a low of 28°C (82°F).
-    There is a possibility of isolated thunderstorms in the afternoon. Friday:
-    It will be partly cloudy, with a high temperature of 34°C (93°F) and a low
-    of 27°C (81°F). There is a chance of showers and thunderstorms in the
-    evening. Saturday: Expect a mix of sun and clouds, with a high of 33°C
-    (91°F) and a low of 26°C (79°F). There is a slight chance of afternoon
-    showers. Sunday: The week ends with mostly sunny skies and a high
-    temperature of 32°C (90°F). The low will be around 26°C (79°F) with a
-    minimal chance of rain. Overall, it looks like Singapore will experience
-    mostly sunny and hot weather throughout the week, with a slight chance of
-    showers and thunderstorms on some days. So make sure to stay hydrated and
-    take necessary precautions to beat the heat. Conclusion In conclusion,
-    staying informed about the latest weather forecast is essential for planning
-    your day and ensuring your safety. By understanding the upcoming weather
-    conditions, you can make informed decisions about what to wear, whether to
-    carry an umbrella, or if you should reschedule outdoor activities. Remember
-    to check for any updates or alerts throughout the day, as weather conditions
-    can change rapidly. Stay prepared and stay safe!
+    Wednesday: Another day of sunshine is in store&#39; with temperatures
+    reaching a high of 34°C (93°F) and a low of 28°C (82°F). No rain is
+    expected. Thursday: The heat continues&#39; with a high of 35°C (95°F) and a
+    low of 28°C (82°F). There is a possibility of isolated thunderstorms in the
+    afternoon. Friday: It will be partly cloudy&#39; with a high temperature of
+    34°C (93°F) and a low of 27°C (81°F). There is a chance of showers and
+    thunderstorms in the evening. Saturday: Expect a mix of sun and clouds&#39;
+    with a high of 33°C (91°F) and a low of 26°C (79°F). There is a slight
+    chance of afternoon showers. Sunday: The week ends with mostly sunny skies
+    and a high temperature of 32°C (90°F). The low will be around 26°C (79°F)
+    with a minimal chance of rain. Overall&#39; it looks like Singapore will
+    experience mostly sunny and hot weather throughout the week&#39; with a
+    slight chance of showers and thunderstorms on some days. So make sure to
+    stay hydrated and take necessary precautions to beat the heat. Conclusion In
+    conclusion&#39; staying informed about the latest weather forecast is
+    essential for planning your day and ensuring your safety. By understanding
+    the upcoming weather conditions&#39; you can make informed decisions about
+    what to wear&#39; whether to carry an umbrella&#39; or if you should
+    reschedule outdoor activities. Remember to check for any updates or alerts
+    throughout the day&#39; as weather conditions can change rapidly. Stay
+    prepared and stay safe!
   </p>
 );
 
@@ -129,8 +134,8 @@ export default function page() {
         <div className="md:flex space-x-12  p-2 md:p-4 mb-20 mx-auto max-w-6xl">
           <div className="max-w-[70%] space-y-3">
             <h2 className="text-2xl font-bold">
-              Another Storm to Form in the Bay of Bengal This Weekend, Just a
-              Week After Cyclone Midhili’s Rampage!
+              Another Storm to Form in the Bay of Bengal This Weekend&#39; Just
+              a Week After Cyclone Midhili’s Rampage!
             </h2>
             <p className="bg-slate-100 rounded-md w-fit px-2">4 hours ago</p>
             <Image
