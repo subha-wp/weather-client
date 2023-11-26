68cb8714f9bde28ae5195fc38bdd21684bd5dc6d
Subha Patra <56377135+subha-wp@users.noreply.github.com>
2023-11-26 07:59:13 +0530

    update analytics

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,27 @@
 import RecentStories from "@/components/RecentStories";
 
+const title = "Weater 10 Days - Get Live Weather Forecasting ";
+const description =
+  "Quick and reliable source for 10-day weather forecasts. Stay informed on temperature, precipitation, and wind speed to plan your week effortlessly.";
+const keywords =
+  "weather 10 days, weather 10-day, weather in delhi 10 days,weather 10 day, aaj ka mausam, 10 दिनों का मौसम, कल मौसम कैसा रहेगा, weather in manali 10 days, weather in ludhiana 10 days  ";
+const url = "https://weather10days.com/";
+
+export const metadata = {
+  title: title,
+  description: description,
+  keywords: keywords,
+  openGraph: {
+    title: title,
+    description: description,
+    keywords: keywords,
+    siteName: "Weather 10 days",
+    url: url,
+    type: "website",
+    images: ["./weather-10-days.jpg"],
+  },
+};
+
 export default function Home() {
   return (
     <main className="my-12 mx-auto max-w-6xl px-2">
--- components/SetCookie.js
+++ components/SetCookie.js
@@ -16,7 +16,7 @@ export default function SetCookie() {
   };
   useEffect(() => {
     fetchLocation();
-  });
+  }, []);
 
   return;
   null;
