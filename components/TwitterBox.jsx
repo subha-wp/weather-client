c23d25ef18309c17082cd07e293512871ac09c79
Subha Patra <56377135+subha-wp@users.noreply.github.com>
2023-11-30 08:56:45 +0530

    Update TwitterBox.jsx

--- components/TwitterBox.jsx
+++ components/TwitterBox.jsx
@@ -5,18 +5,7 @@ export default async function TwitterBox({ posts }) {
   return (
     <div>
       <h3>Recent Tweets</h3>
-      <div className="overflow-x-auto w-6xl flex items-start space-x-3">
-        {posts.map((post, idx) => {
-          //   console.log(post);
-          const id = post.title.rendered;
-          return (
-            <div key={idx} className="w-[350px]">
-              <Tweet id={id} />
-            </div>
-          );
-        })}
-      </div>
-      <div className="md:hidden space-y-3">
+      <div className="overflow-x-auto w-6xl flex items-start space-x-3 overflow-hidden">
         {posts.map((post, idx) => {
           //   console.log(post);
           const id = post.title.rendered;
